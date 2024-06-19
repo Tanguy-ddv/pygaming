395f5b325c4fe3cf6c1fe6dc178c91b16689831e
Tanguy Dugas du Villard <tanguy.dugas01@gmail.com>
2024-06-19 12:58:32 +0200

    new: dynamic and static files, database. install to set the path to dynamic files.

---
+++ pygaming/commands/setup.py
@@ -0,0 +1,11 @@
+import os
+import json
+from .install import install
+abs_path = os.path.abspath('.')
+config_path = abs_path + '/data/config.json'
+with open(config_path, 'r', encoding='utf-8') as f:
+    config = json.load(f)
+    config['path_to_files'] = abs_path
+with open(config_path, 'w', encoding='utf-8') as f:
+    json.dump(config, f)
+install()
--- pygaming/io_/database/database.py
+++ pygaming/io_/database/database.py
@@ -3,38 +3,63 @@ The Database is used to address queries to the database.
 """
 import sqlite3 as sql
 import os
+from .._utils import get_file
+
+__DB_PATH = get_file('data','sql/db.sqlite')
+__TABLE_PATH = get_file('data', 'sql/tables.sql')
+__IG_QUERIES_PATH = get_file('data', 'sql/ig_queries.sql')
+__IG_STATIC_ = get_file('data', 'sql/ig_queries.sql',)
 
 class Database:
-    """The Database is used to adress queries to the database."""
+    """The Database instance is used to adress queries to the database.
+        No need to have 2 databases on the same code.
+    """
 
-    def __init__(self, db_path: str, table_sql_path: str, script_path_folder, in_game_script_path: str, debug: bool=False) -> None:
+    def __init__(self, debug: bool=False) -> None:
         """
-        Initialize an instance of Databas.
+        Initialize an instance of Database.
         
         args:
-        db_path: The path to the database.
-        table_sql_path: the path to the sql file that creates tables.
-        script_path_folder: the folder containging all the sql files to be executed.
-        in_game_script_path: The path to the sql file that stores every insert queries executed during the games.
         debug: when passed to true, the delation of the database is not done at the destruction of the instance.
         """
-        self._db_path = db_path
+
         self._debug = debug
-        self._in_game_script_path = in_game_script_path
-        self._create_database(table_sql_path, script_path_folder, in_game_script_path)
+        # Remove the previous sqlite file if existing.
+        if os.path.isfile(__DB_PATH):
+            os.remove(__DB_PATH)
+        
+        # Create and connect to the sqlite file.
+        conn = sql.connect(__DB_PATH)
+        conn.close()
+
+        # Initialize the sqlite file with the tables.
+
+        self._execute_sql_script(__TABLE_PATH)
+
+        for root, _, files in os.walk(get_file('data'), '/'):
+            for file in files:
+                complete_path = os.path.join(root, file).replace('\\', '/')
+                if complete_path.endswith('.sql') and complete_path != __TABLE_PATH and complete_path != __IG_QUERIES_PATH:
+                    if self._debug:
+                        print(complete_path)
+                    self._execute_sql_script(complete_path)
+        
+        # Execute the queries previously saved.
+        self._execute_sql_script(__IG_QUERIES_PATH)
 
     def _execute_select_query(self, query: str):
         """Execute a select query on the database."""
         if not query.startswith("SELECT"):
             print("The query is wrong:\n", query, "\nShould start by 'SELECT'")
-        if not os.path.isfile(self._db_path):
+        if not os.path.isfile(__DB_PATH):
             print("The database has not been created yet.")
+            return None, None
         try:
-            conn = sql.connect(self._db_path)
+            conn = sql.connect(__DB_PATH)
             cur = conn.cursor()
             cur.execute(query)
             result = cur.fetchall()
-            description = [description[0] for description in cur.description]
+            description = [descr[0] for descr in cur.description]
             cur.close()
             conn.close()
             return result, description
@@ -46,16 +71,19 @@ class Database:
         if not query.startswith("INSERT INTO"):
             print("The query is wrong:\n", query, "\nShould start by 'INSERT INTO'")
             return None
-        if not os.path.isfile(self._db_path):
+        if not os.path.isfile(__DB_PATH):
             print("The database has not been created yet.")
             return None
         try:
-            conn = sql.connect(self._db_path)
+            # Execute the query on the Database
+            conn = sql.connect(__DB_PATH)
             cur = conn.cursor()
             cur.execute(query)
-            with open(self._in_game_script_path, 'a', encoding='utf-8') as f:
-                f.write(";\n" + query)
             conn.commit()
+            # Save the query on the ig_query file to execute it every time you launch the app.
+            in_game_query_saved = get_file('data', 'sql/ig_queries.sql')
+            with open(in_game_query_saved, 'a', encoding='utf-8') as f:
+                f.write(";\n" + query)
             cur.close()
             conn.close()
         except sql.Error as error:
@@ -64,7 +92,7 @@ class Database:
     def _execute_sql_script(self, script_path: str):
         """Execute a script query on the database."""
         try:
-            conn = sql.connect(self._db_path)
+            conn = sql.connect(__DB_PATH)
             cur = conn.cursor()
             with open(script_path, 'r', encoding='utf-8') as f:
                 script = f.read()
@@ -77,36 +105,10 @@ class Database:
         except sql.Error as error:
             print("An error occured while querying the database with the script located at\n",script_path,"\n",error)            
 
-    def _create_database(self, table_sql_path: str, script_path_folder: str, in_game_script_path: str):
-        """
-        Create the database and execute the scripts in it.
-
-        table_sql_path: the path to the sql file that creates tables.
-        script_path_folder: the path to the folder containing all the scripts.
-        """
-        if os.path.isfile(self._db_path):
-            os.remove(self._db_path)
-
-        conn = sql.connect(self._db_path)
-        conn.close()
-
-        self._execute_sql_script(table_sql_path)
-
-        for root, _, files in os.walk(script_path_folder):
-            for file in files:
-                complete_path = os.path.join(root, file)
-                if complete_path.endswith('.sql') and complete_path.replace('\\','/') != table_sql_path and complete_path.replace('\\','/') != in_game_script_path:
-                    if self._debug:
-                        print(complete_path)
-                    self._execute_sql_script(complete_path)
-        
-        self._execute_sql_script(in_game_script_path)
-
-
     def __del__(self):
-        """Destroy the DatabaseManager. Delete the database"""
-        if os.path.isfile(self._db_path) and not self._debug:
-            os.remove(self._db_path)
+        """Destroy the Database object. Delete the database file"""
+        if os.path.isfile(__DB_PATH) and not self._debug:
+            os.remove(__DB_PATH)
 
     def get_data_by_id(self, id_: int, table: str, return_id: bool = True):
         """Get all the data of one row based on the id and the table."""
@@ -115,8 +117,7 @@ class Database:
         return {key : value for key,value in zip(description, result[0]) if (key != f"{table}_id" or return_id)}
     
     def get_collection_joined_by_id(self, id_, table: str, join_table: str, return_id: bool = True) -> list:
-        """Get all the row of a the table 'table' having has foreign key for the table 'join_table' the id_"""
+        """Get all the row of a the table 'table' having has foreign key for the table 'join_table' the id id_"""
         query = f"SELECT * FROM {table} WHERE {join_table}_id = {id_}"
         result, description = self._execute_select_query(query)
         return [{key : value for key,value in zip(description, res) if (key != f"{table}_id" or return_id)} for res in result]
-    
