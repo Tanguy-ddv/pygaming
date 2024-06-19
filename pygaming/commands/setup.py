80291f45b901eacf1c74dd78680ac71eb8bd5efc
Tanguy Dugas du Villard <tanguy.dugas01@gmail.com>
2024-06-19 14:04:20 +0200

    fix: unmatching keys for path_to_dynamic

--- pygaming/commands/setup.py
+++ pygaming/commands/setup.py
@@ -5,7 +5,7 @@ abs_path = os.path.abspath('.')
 config_path = abs_path + '/data/config.json'
 with open(config_path, 'r', encoding='utf-8') as f:
     config = json.load(f)
-    config['path_to_files'] = abs_path
+    config['path_to_dynamic'] = abs_path
 with open(config_path, 'w', encoding='utf-8') as f:
     json.dump(config, f)
 install()
