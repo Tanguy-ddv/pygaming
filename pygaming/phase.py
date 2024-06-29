538ed2418b069b84e5e2d6ae86eb55640cb6a9e2
Tanguy Dugas du Villard <tanguy.dugas01@gmail.com>
2024-06-29 23:30:30 +0200

    new: start of creation of phases and frame

---
+++ pygaming/phase.py
@@ -0,0 +1,27 @@
+"""A phase is one step of the game."""
+import pygame
+from .ui.frame import Frame
+from .ui.inputs import Inputs, Click
+
+class Phase():
+    """
+    A Phase is a step in these. Each game should have a few phases.
+    Exemple of phases: menus, lobby, stages, ...
+    A phase is composed by a set of frames
+    """
+
+    def __init__(self, inputs: Inputs) -> None:
+        self.frames: list[Frame] = []
+        self.inputs = inputs
+    
+    def add_frame(self, frame: Frame):
+        self.frames.append(frame)
+    
+    def update_focus(self):
+        clicks = self.inputs.get_clicks()
+        if 1 in clicks and not clicks[1].up:
+            x = clicks[1].x
+            y = clicks[1].y
+            for frame in self.frames:
+                if frame.x < x < frame.x + frame.width and frame.y < y < frame.y + frame.height:
+                    frame.update_focus(x, y)
---
+++ pygaming/ui/frame.py
@@ -0,0 +1,48 @@
+from .widget.base_widget import BaseWidget
+import pygame
+from ..utils.color import Color
+from .inputs import Inputs
+
+class Frame:
+
+    def __init__(self, x :int, y:int, width: int, height: int, background: pygame.Surface | Color = Color(255,255,255)) -> None:
+        self.widgets: list[BaseWidget] = []
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        
+        if isinstance(background, Color):
+            self.background = pygame.Surface((width, height))
+            self.background.fill(background.to_RGBA())
+        else:
+            self.background = background.subsurface((0,0, width, height))
+    
+    @property
+    def shape(self):
+        return (self.width, self.height)
+
+    @property
+    def top_left(self):
+        return (self.x, self.y)
+
+    @property
+    def rect(self):
+        return self.x, self.y, self.x + self.width, self.y + self.height
+
+    def add_widget(self, widget: BaseWidget):
+        self.widgets.append(widget)
+    
+    def update_focus(self, click_x, click_y):
+        """Update the focus of all the widgets in the phase."""
+        for widget in self.widgets:
+            if widget.visible:
+                if widget.x < click_x < widget.x + widget.width and widget.y < click_y < widget.y + widget.height:
+                    widget.focus()
+                else:
+                    widget.unfocus()
+    
+    def update_widgets(self, inputs: Inputs, loop_duration: int):
+        """Update all the widgets."""
+        for widget in self.widgets:
+            widget.update(inputs, loop_duration)
