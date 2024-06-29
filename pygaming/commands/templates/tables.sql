3f56776c77477ebce796291e47d22f4145f41479
Tanguy Dugas du Villard <tanguy.dugas01@gmail.com>
2024-06-29 18:16:39 +0200

    minor fixes

--- pygaming/commands/templates/tables.sql
+++ pygaming/commands/templates/tables.sql
@@ -2,14 +2,14 @@
 
 CREATE TABLE localizations (
     position TEXT NOT NULL, --"LOC_..."
-    language_code TEXT DEFAULT 'en_US', --'en_US" for us english, "fr_FR" for french, "it_IT" for italian, "es_MX" for mexican spanish etc.
+    language_code TEXT, --'en_US" for us english, "fr_FR" for french, "it_IT" for italian, "es_MX" for mexican spanish etc.
     text_value TEXT NOT NULL -- The value itself
 );
 
 -- Exemple of table to store the users:
 
 CREATE TABLE player(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    id AUTOINCREMENT PRIMARY KEY,
     name TEXT NOT NULL,
     victories INTEGER DEFAULT 0
 )
---
+++ pygaming/ui/sound/__init__.py
@@ -0,0 +1,4 @@
+from .music import Jukebox
+from .sounds import Sound, SoundBox
+
+__all__ = ['Jukebox', 'Sound', 'SoundBox']
