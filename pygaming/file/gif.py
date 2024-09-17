59e224e01dcd44ea7f76aa3c0dfa4382acd6869c
tanguy-ddv <tanguy.dugas01@gmail.com>
2024-09-18 00:14:28 +0200

    new: use lazy computation to save space by using as little ressources as possible during file creation and moving the use to the get method

--- pygaming/file/gif.py
+++ pygaming/file/gif.py
@@ -4,34 +4,32 @@ import pygame
 from PIL import Image
 from .file import get_file, File
 
-# import a gif holder and use it to create a list of files.
-# do the exact same thing as would be a list of images
-# from imagefiles.
-
 class GIFFile(File):
     """A GifFile represent a GIF in the assets/images folder."""
 
     def __init__(self, path: str) -> None:
         super().__init__(path)
         self.full_path = get_file('images', path)
+
+    def get(self, size: tuple[int, int] | None = None, rotation: float = 0):
+        """Get the gif as a list of surfaces and frame durations."""
         gif = Image.open(self.full_path)
         gif.seek(0)
-        self._images: list[pygame.Surface] = [pygame.image.fromstring(gif.convert('RGBA').tobytes(), gif.size, 'RGBA')]
-        self._frame_durations = [gif.info['duration']]
+        images: list[pygame.Surface] = [pygame.image.fromstring(gif.convert('RGBA').tobytes(), gif.size, 'RGBA')]
+        frame_durations = [gif.info['duration']]
         while True:
             try:
                 gif.seek(gif.tell()+1)
-                self._images.append(pygame.image.fromstring(gif.convert('RGBA').tobytes(), gif.size, 'RGBA'))
-                self._frame_durations.append(gif.info['duration'])
+                images.append(pygame.image.fromstring(gif.convert('RGBA').tobytes(), gif.size, 'RGBA'))
+                frame_durations.append(gif.info['duration'])
             except EOFError:
                 break
 
-    def get(self, size: tuple[int, int] | None = None, rotation: float = 0):
         images_to_return = []
-        for im in self._images:
+        for im in images:
             surface = pygame.transform.rotate(im, rotation)
             if size is None:
                 images_to_return.append(surface)
             else:
                 images_to_return.append(pygame.transform.scale(im, size))
-        return images_to_return, self._frame_durations
+        return images_to_return, frame_durations
