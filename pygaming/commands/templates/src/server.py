44df514388d6062df2ac8f9de9634c5da279c7bf
tanguy-ddv <tanguy.dugas01@gmail.com>
2024-09-30 18:42:15 +0200

    chg: update template

--- pygaming/commands/templates/src/server.py
+++ pygaming/commands/templates/src/server.py
@@ -1,23 +1,26 @@
 """Use this file to create a server."""
-from typing import Any
-from pygaming import Server, ServerPhase, ServerTransition
+from pygaming import Server, ServerPhase, STAY
 
 class ServerLobby(ServerPhase):
-    
+
     def __init__(self, server: Server) -> None:
         super().__init__("lobby", server)
-    
+
     def start(self):
         pass
 
-    def _update(self, loop_duration: int):
+    def update(self, loop_duration: int):
         pass
 
     def next(self):
         return ''
-    
+
     def end(self):
         self.network.send_all("match start", {"color1" : "blue", "color2" : "red"})
+
+    def apply_transition(self, next_phase: str):
+        return {"color1" : "blue", "color2" : "red"}
+
 
 class ServerMacth(ServerPhase):
 
@@ -27,26 +30,20 @@ class ServerMacth(ServerPhase):
     def start(self, color1: str, color2: str):
         pass
 
-    def _update(self, loop_duration: int):
+    def update(self, loop_duration: int):
         pass
 
     def next(self):
-        return ''
-    
+        return STAY
+
     def end(self):
         pass
 
-class ServerLobbyMatchTransition(ServerTransition):
-
-    def __init__(self, server: Server) -> None:
-        super().__init__(server, "lobby", "match")
-
-    def apply(self, phase: ServerLobby) -> dict[str, Any]:
-        return {"color1" : "blue", "color2" : "red"}
+    def apply_transition(self, next_phase: str):
+        pass
 
 if __name__ == '__main__':
-    server = Server(debug = False, nb_max_player = 6)
-    ServerLobby(server)
-    ServerMacth(server)
-    ServerLobbyMatchTransition(server)
-    server.run()
+    serv = Server(nb_max_player = 6, first_phase='lobby', debug=True)
+    ServerLobby(serv)
+    ServerMacth(serv)
+    serv.run()
