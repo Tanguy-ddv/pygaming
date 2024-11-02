c60f2790b348551b3db85ef3a05030378fe34684
tanguy-ddv <tanguy.dugas01@gmail.com>
2024-11-02 14:18:46 +0100

    new: add support for folders in playlist read

--- pygaming/music.py
+++ pygaming/music.py
@@ -1,6 +1,8 @@
 """The Jukebox class is used to manage the musics."""
 
 from random import shuffle
+from typing import Union
+import os
 import pygame
 from .settings import Settings
 from .file import get_file
@@ -13,12 +15,12 @@ class Jukebox:
 
     def __init__(self, settings: Settings) -> None:
 
-        self._loop_instant = 0
+        self._introduction = 0
         self._playing = False
         self._settings = settings
         self._loops_or_playlist = _LOOPS
         self._playlist_idx = 0
-        self._playlist_playing = []
+        self._playlist = []
 
     def stop(self):
         """Stop the music currently playing."""
@@ -35,29 +37,60 @@ class Jukebox:
         pygame.mixer.music.unpause()
         self._playing = True
 
-    def play_loop(self, path: str, loop_instant: int):
-        """Play a music that will loop."""
+    def play_loop(self, path: str, introduction: int = 0):
+        """
+        Play a music that will loop.
+        
+        Params:
+        ----
+        - path: str, the path to the music file in the assets/musics/ folder
+        - introduction: int (ms), the duration of the introduction of the music. 
+
+        Example:
+        -----
+
+        >>> jukebox.play_loop("my_music.mp3", 5000)
+        will play assets/musics/my_music.mp3. Once the music is over, resume the play at the instant 5000 ms
+        """
         full_path = get_file('musics', path)
-        self._loop_instant = loop_instant
+        self._introduction = introduction
         self._loops_or_playlist = _LOOPS
         pygame.mixer.music.load(full_path)
         pygame.mixer.music.play(0)
         self._playing = True
         self._playlist_idx = 0
     
-    def read_playlist(self, playlist: list[str], random: bool = False):
-        """Play a playlist"""
+    def read_playlist(self, playlist: Union[str,list[str]], random: bool = False):
+        """
+        Play a playlist.
+
+        Params:
+        -----
+        - playlist: str, list[str]. A folder or a list of file in the assets/musics folder
+        - random: bool, if True, randomize the playlist to be played
+        
+        Examples:
+        -----
+        >>> jukebox.read_playlist('my_playlist/', '')
+        """
+
         self._loops_or_playlist = _PLAYLIST
         self._playlist_idx = 0
+        if isinstance(playlist, str):
+            # We have a folder
+            folder = get_file('musics', playlist)
+            self._playlist = [os.path.join(folder, f) for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+        else:
+            # We have a list of paths
+            self._playlist = [get_file('musics', path) for path in playlist]
         if random:
-            shuffle(playlist)
-        self._playlist_playing = [get_file('musics', path) for path in playlist]
+            shuffle(self._playlist)
         self._playing = True
     
     def add_to_playlist(self, path: str):
         """Add a music to the playlist."""
         full_path = get_file('musics', path)
-        self._playlist_playing.append(full_path)
+        self._playlist.append(full_path)
 
     def update(self):
         """This function should be called at the end of every gameloop to make the music loop or the jukebox play a new music."""
@@ -68,7 +101,7 @@ class Jukebox:
 
         # If we are reading a playlist
         if self._playing and self._loops_or_playlist == _PLAYLIST and not pygame.mixer.music.get_busy():
-            self._playlist_idx = (self._playlist_idx+1)%len(self._playlist_playing)
-            path = self._playlist_playing[self._playlist_idx]
+            self._playlist_idx = (self._playlist_idx+1)%len(self._playlist)
+            path = self._playlist[self._playlist_idx]
             pygame.mixer.music.load(path)
             pygame.mixer.music.play(0)
