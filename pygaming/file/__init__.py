1c4866d9e134a83c8d152b8e556ff035c4dbc945
tanguy-ddv <tanguy.dugas01@gmail.com>
2024-11-02 14:13:37 +0100

    chg: remove the music file imports

--- pygaming/file/__init__.py
+++ pygaming/file/__init__.py
@@ -2,7 +2,6 @@
 from .font import FontFile, default_font
 from .data import DataFile
 from .image import ImageFile
-from .music import MusicFile
 from .file import get_file
 from .gif import GIFFile
-__all__ = ['FontFile', 'default_font', 'MusicFile', 'ImageFile', 'DataFile', 'get_file', 'GIFFile']
+__all__ = ['FontFile', 'default_font', 'ImageFile', 'DataFile', 'get_file', 'GIFFile']
