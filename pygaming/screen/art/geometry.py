5d547a6e048d614266bd541c14b52a2ba6b0d76a
tanguy-ddv <tanguy.dugas01@gmail.com>
2024-11-25 21:45:28 +0100

    new: add TexturedRoundedRectangle, TexturedEllipse and TexturedCicle based on pygame masks

--- pygaming/screen/art/geometry.py
+++ pygaming/screen/art/geometry.py
@@ -1,7 +1,7 @@
 """The colored_surface module contains the ColoredSurface class which is a pygame Surface."""
 
 from typing import Sequence
-from pygame import Surface, SRCALPHA, draw, gfxdraw
+from pygame import Surface, SRCALPHA, draw, gfxdraw, mask as msk
 from ...color import ColorLike
 from .art import Art
 from .transformation import Transformation
@@ -80,7 +80,7 @@ class ColoredCircle(Art):
         self.durations = (0,)
 
 class ColoredEllipse(Art):
-    """A ColoredEllipse is an Art with a colored ellipse at the center."""
+    """A ColoredEllipse is an Art with a colored ellipsis at the center."""
 
     def __init__(self, color: ColorLike, horizontal_radius: int, vertical_radius: int,
             thickness: int = 0, transformation: Transformation = None, force_load_on_start: bool = False) -> None:
@@ -129,6 +129,7 @@ class ColoredPolygon(Art):
         self.durations = (0,)
 
 class TexturedPolygon(Art):
+    """A Textured polygon is a polygon filled with an art as texture."""
 
     def __init__(
         self,
@@ -169,6 +170,141 @@ class TexturedPolygon(Art):
         self.surfaces = tuple(surfaces)
         self.durations = self.texture.durations
         self.introduction = self.texture.introduction
+
+        if need_to_unload:
+            self.texture.unload()
+
+class TexturedCircle(Art):
+    """A TexturedCircle is an Art with a textured circle at the center of it."""
+
+    def __init__(
+        self,
+        radius: int,
+        texture: Art,
+        center: tuple[int, int] = None,
+        draw_top_right: bool = False,
+        draw_top_left: bool = False,
+        draw_bottom_left: bool = False,
+        draw_bottom_right: bool = False,
+        transformation: Transformation = None,
+        force_load_on_start: bool = False
+    ):
+        super().__init__(transformation, force_load_on_start)
+        self.radius = radius
+        self.draw_top_right = draw_top_right
+        self.draw_top_left = draw_top_left
+        self.draw_bottom_left = draw_bottom_left
+        self.draw_bottom_right = draw_bottom_right
+        if center is None:
+            center = texture.width//2, texture.height//2
+        self.center = center
+        self._width = texture.width
+        self._height = texture.height
+        self.texture = texture
+
+        self._find_initial_dimension()
+    
+    def _load(self):
+
+        if not self.texture.is_loaded:
+            need_to_unload = True
+            self.texture.load()
+
+        surf = Surface((self._width, self._height), SRCALPHA)
+        draw.circle(surf, (255, 255, 255, 255), self.center,
+            self.radius, 0, self.draw_top_right, self.draw_top_left, self.draw_bottom_left, self.draw_bottom_right)
+        mask = msk.from_surface(surf, 127)
+        self.surfaces = (mask.to_surface(setsurface=surface) for surface in self.texture.surfaces)
+        self.durations = self.texture.durations
+    
+        if need_to_unload:
+            self.texture.unload()
+
+class TexturedEllipse(Art):
+    """A TexturedEllipse is an Art with a textured ellipsis at the center of it."""
+
+    def __init__(
+        self,
+        horizontal_radius: int,
+        vertical_radius: int,
+        texture: Art,
+        center: tuple[int, int] = None,
+        transformation: Transformation = None,
+        force_load_on_start: bool = False
+    ) -> None:
+        super().__init__(transformation, force_load_on_start)
+        if center is None:
+            center = texture.width//2, texture.height//2
+        self.center = center
+        self.rect = (self.center[0] - horizontal_radius, self.center[0] - vertical_radius, horizontal_radius*2, vertical_radius*2)
+        self._width = texture.width
+        self._height = texture.height
+        self.texture = texture
+        self._find_initial_dimension()
+    
+    def _load(self):
+
+        if not self.texture.is_loaded:
+            need_to_unload = True
+            self.texture.load()
+
+        surf = Surface(self.rect[2:4], SRCALPHA)
+        draw.ellipse(surf, (255, 255, 255, 255), self.rect, 0)
+        mask = msk.from_surface(surf, 127)
+        self.surfaces = (mask.to_surface(setsurface=surface) for surface in self.texture.surfaces)
+        self.durations = self.texture.durations
+
+        if need_to_unload:
+            self.texture.unload()
+
+class TexturedRoundedRectangle(Art):
+    """A TexturedRoundedRectangle is an Art with rounded angles."""
+
+    def __init__(
+        self,
+        texture: Art,
+        border_radius: int = 0,
+        border_top_left_radius: int = -1,
+        border_top_right_radius: int = -1,
+        border_bottom_left_radius: int = -1,
+        border_bottom_right_radius: int = -1,
+        transformation: Transformation = None,
+        force_load_on_start: bool = False
+    ):
+        super().__init__(transformation, force_load_on_start)
+        self.border_radius = border_radius
+        self.border_top_left_radius = border_top_left_radius
+        self.border_top_right_radius = border_top_right_radius
+        self.border_bottom_left_radius = border_bottom_left_radius
+        self.border_bottom_right_radius = border_bottom_right_radius
+
+        self._height = texture.height
+        self._width = texture.width
+        self.texture = texture
+
+        self._find_initial_dimension()
+
+    def _load(self):
+         
+        if not self.texture.is_loaded:
+            need_to_unload = True
+            self.texture.load()
+
+        surf = Surface((self.width, self.height), SRCALPHA)
+        draw.rect(
+            surf,
+            (255, 255, 255, 255),
+            (0, 0, self.width, self.height),
+            0,
+            self.border_radius,
+            self.border_top_left_radius,
+            self.border_top_right_radius,
+            self.border_bottom_left_radius,
+            self.border_bottom_right_radius
+        )
+        mask = msk.from_surface(surf, 127)
+        self.surfaces = (mask.to_surface(setsurface=surface) for surface in self.texture.surfaces)
+        self.durations = self.texture.durations
 
         if need_to_unload:
             self.texture.unload()
