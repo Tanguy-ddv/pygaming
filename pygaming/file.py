fa444436ae7be56035fc0a3fa7d3da914f0f4cb8
tanguy-ddv <tanguy.dugas01@gmail.com>
2024-12-29 23:21:15 +0100

    new: add cache to optimize the base path retrival

--- pygaming/file.py
+++ pygaming/file.py
@@ -5,6 +5,17 @@ from typing import Literal
 
 import os
 import json
+from functools import lru_cache
+
+@lru_cache(maxsize=1)
+def _get_base_path():
+
+    config_path = os.path.join(os.path.abspath("."), 'data', 'config.json')
+    config_file = open(config_path,'r', encoding='utf-8')
+    base_path = json.load(config_file)['path']
+    config_file.close()
+
+    return base_path
 
 def get_file(folder: Literal['data', 'musics', 'sounds', 'images', 'videos', 'fonts'], file: str):
     """
@@ -16,10 +27,8 @@ def get_file(folder: Literal['data', 'musics', 'sounds', 'images', 'videos', 'fo
     - file: the name of the file.
     """
 
-    config_path = os.path.join(base_path, 'data', 'config.json')
-    config_file = open(config_path,'r', encoding='utf-8')
-    base_path = json.load(config_file)['path']
-    config_file.close()
+    base_path = _get_base_path()
+
     if folder != 'data':
         return os.path.join(base_path, 'assets', folder, file).replace('\\', '/')
     else:
