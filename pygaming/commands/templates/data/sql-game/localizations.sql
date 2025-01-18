8cca00d39de0e7c4ab0107574c25e23c227a1997
tanguy-ddv <tanguy.dugas01@gmail.com>
2025-01-18 11:43:58 +0100

    new: add tags for phases to avoid duplicate lines

--- pygaming/commands/templates/data/sql-game/localizations.sql
+++ pygaming/commands/templates/data/sql-game/localizations.sql
@@ -1,4 +1,4 @@
-INSERT INTO localizations (position, phase_name, language_code, text_value) VALUES 
+INSERT INTO localizations (position, phase_name_or_tag, language_code, text_value) VALUES 
 ('LOC_SELECT_COLOR', 'lobby', 'en_US', 'SELECT A COLOR'),
 ('LOC_SELECT_COLOR', 'lobby', 'fr_FR', 'SELECTIONNER UNE COULEUR'),
 ('LOC_SELECT_NAME' , 'lobby', 'en_US', 'SELECT A NAME:'),
@@ -11,20 +11,20 @@ INSERT INTO localizations (position, phase_name, language_code, text_value) VALU
 ('LOC_SETTINGS','all', 'fr_FR', 'PARAMETRES'),
 ('LOC_RESUME',  'playground', 'en_US', 'RESUME'),
 ('LOC_RESUME',  'playground', 'fr_FR', 'REPRENDRE'),
-('LOC_VALIDATE_SETTINGS', 'all', 'en_US', 'VALIDATE'),
-('LOC_VALIDATE_SETTINGS', 'all', 'fr_FR', 'VALIDER'),
-('LOC_FULLSCREEN_ON',     'all', 'en_US', 'FULLSCREEN: ON'),
-('LOC_FULLSCREEN_ON',     'all', 'fr_FR', 'ACTIVER PLEIN ECRAN'),
-('LOC_FULLSCREEN_OFF',    'all', 'en_US', 'FULLSCREEN: OFF'),
-('LOC_FULLSCREEN_OFF',    'all', 'fr_FR', 'DESACTIVER PLEIN ECRAN'),
-('LOC_LANGUAGE_EN',       'all', 'en_US', 'LANGUAGE: EN'),
-('LOC_LANGUAGE_EN',       'all', 'fr_FR', 'ANGLAIS'),
-('LOC_LANGUAGE_FR',       'all', 'en_US', 'LANGUAGE: FR'),
-('LOC_LANGUAGE_FR',       'all', 'fr_FR', 'FRANCAIS'),
-('LOC_CONTROLS_NORMAL',   'all', 'en_US', 'CONTROLS: NORMAL'),
-('LOC_CONTROLS_NORMAL',   'all', 'fr_FR', 'CONTROLES NORMAUX'),
-('LOC_CONTROLS_INVERTED', 'all', 'en_US', 'CONTROLS: INVERTED'),
-('LOC_CONTROLS_INVERTED', 'all', 'fr_FR', 'CONTROLES INVERSES')
+('LOC_VALIDATE_SETTINGS', 'settings', 'en_US', 'VALIDATE'),
+('LOC_VALIDATE_SETTINGS', 'settings', 'fr_FR', 'VALIDER'),
+('LOC_FULLSCREEN_ON',     'settings', 'en_US', 'FULLSCREEN: ON'),
+('LOC_FULLSCREEN_ON',     'settings', 'fr_FR', 'ACTIVER PLEIN ECRAN'),
+('LOC_FULLSCREEN_OFF',    'settings', 'en_US', 'FULLSCREEN: OFF'),
+('LOC_FULLSCREEN_OFF',    'settings', 'fr_FR', 'DESACTIVER PLEIN ECRAN'),
+('LOC_LANGUAGE_EN',       'settings', 'en_US', 'LANGUAGE: EN'),
+('LOC_LANGUAGE_EN',       'settings', 'fr_FR', 'ANGLAIS'),
+('LOC_LANGUAGE_FR',       'settings', 'en_US', 'LANGUAGE: FR'),
+('LOC_LANGUAGE_FR',       'settings', 'fr_FR', 'FRANCAIS'),
+('LOC_CONTROLS_NORMAL',   'settings', 'en_US', 'CONTROLS: NORMAL'),
+('LOC_CONTROLS_NORMAL',   'settings', 'fr_FR', 'CONTROLES NORMAUX'),
+('LOC_CONTROLS_INVERTED', 'settings', 'en_US', 'CONTROLS: INVERTED'),
+('LOC_CONTROLS_INVERTED', 'settings', 'fr_FR', 'CONTROLES INVERSES')
 
 -- Example of use of the localizations table
 -- in the game, get the text via self.texts.get(position).
