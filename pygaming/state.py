7d0277f09326e7bac241587c03a3a5f8e6a6ae2b
tanguy-ddv <tanguy.dugas01@gmail.com>
2025-01-20 23:11:01 +0100

    new: create a state class

--- pygaming/state.py
+++ pygaming/state.py
@@ -1,84 +1,61 @@
 """The state module contains functions to interact with the state file."""
-import json
-import time
-from functools import lru_cache
 from typing import Any
+import pygame
+from .file import load_json_file, save_json_file
 
-from .file import get_file
+class State:
 
-@lru_cache(maxsize=1)
-def _get_state_path():
-    """
-    Get the path of the state file.
-    """
-    return get_file('data', 'state.json')
+    def __init__(self):
+        self._data, self._encoding = load_json_file('state.json')
 
-def get_state():
-    """
-    Return the content of the state file.
-    """
-    state_path = _get_state_path()
-    with open(state_path, 'r', encoding='utf-8') as f:
-        current_state: dict = json.load(f)
-    return current_state
+    def get_state(self):
+        """
+        Return the current state.
+        """
+        return self._data
 
-def set_state(key: str, value: Any):
-    """
-    Set a new value for one of the entry of the state.
+    def set_state(self, key: str, value: Any):
+        """
+        Set a new value for one of the entry of the state.
 
-    Params:
-    ----
-    - key: str, the name of the attribute to be changed.
-    - value: Any, the value to set the new attribute to.
-    """
-    state_path = _get_state_path()
-    with open(state_path, 'r', encoding='utf-8') as f:
-        current_state: dict = json.load(f)
-    current_state[key] = value
-    with open(state_path, 'w', encoding='utf-8') as f:
-        json.dump(current_state, f, indent=4)
+        Params:
+        ----
+        - key: str, the name of the attribute to be changed.
+        - value: Any, the value to set the new attribute to.
+        """
 
-def set_states(key_values: dict[str, Any]):
-    """
-    Set several new values for the state.
+        self._data[key] = value
 
-    Params:
-    - key_values: dict, a dict of str, Any to update the state to.
-    If some keys of the state are not in this dict, they are unchanged.
+    def set_states(self, key_values: dict[str, Any]):
+        """
+        Set several new values for the state.
 
-    """
-    state_path = _get_state_path()
-    with open(state_path, 'r', encoding='utf-8') as f:
-        current_state: dict = json.load(f)
-    current_state.update(key_values)
-    with open(state_path, 'w', encoding='utf-8') as f:
-        json.dump(current_state, f, indent=4)
+        Params:
+        - key_values: dict, a dict of str, Any to update the state to.
+        If some keys of the state are not in this dict, they are unchanged.
 
-def increment_counter(counter: str):
-    """
-    Increment one counter saved in the state
+        """
+        self._data.update(key_values)
 
-    Params:
-    - counter: str, the name of the counter to increment.
-    """
-    state_path = _get_state_path()
-    with open(state_path, 'r', encoding='utf-8') as f:
-        current_state: dict = json.load(f)
-    current_state[counter] += 1
-    with open(state_path, 'w', encoding='utf-8') as f:
-        json.dump(current_state, f, indent=4)
+    def increment_counter(self, counter: str):
+        """
+        Increment one counter saved in the state
 
-def set_time_now(time_variable: str):
-    """
-    Set a new time for the one variable
+        Params:
+        - counter: str, the name of the counter to increment.
+        """
+        self._data[counter] += 1
 
-    Params:
-    - time_variable: str, the name of the time variable to set to now.
-    time is represent as a timestamp in ms.
-    """
-    state_path = _get_state_path()
-    with open(state_path, 'r', encoding='utf-8') as f:
-        current_state: dict = json.load(f)
-    current_state[time_variable] = int(time.time()*1000)
-    with open(state_path, 'w', encoding='utf-8') as f:
-        json.dump(current_state, f, indent=4)
+    def set_time_now(self, time_variable: str):
+        """
+        Set a new time for the one variable
+
+        Params:
+        - time_variable: str, the name of the time variable to set to now.
+        time is represent as a timestamp in ms.
+        """
+        self._data[time_variable] = pygame.time.get_ticks()
+    
+    def save(self):
+        """Save the current state."""
+        save_json_file("state.json", self._data, self._encoding)
