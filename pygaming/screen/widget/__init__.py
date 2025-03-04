7ca10967230bac0593d2da7b513b0b92fcf48929
tanguy-ddv <tanguy.dugas01@gmail.com>
2025-03-04 15:14:57 +0100

    fix: add forgotten paragraph to all widgets

--- pygaming/screen/widget/__init__.py
+++ pygaming/screen/widget/__init__.py
@@ -1,8 +1,8 @@
 """Pygaming widgets are used to interact with the player."""
 from .button import Button, TextButton
 from .entry import Entry
-from .label import Label
+from .label import Label, Paragraph
 from .slider import Slider
 from .widget import Widget
 
-__all__ = ['Button', 'TextButton', 'Entry', 'Label', 'Slider', 'Widget']
+__all__ = ['Button', 'TextButton', 'Entry', 'Label', 'Slider', 'Widget', 'Paragraph']
