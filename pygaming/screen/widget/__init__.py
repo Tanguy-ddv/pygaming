51d4b33e31594299d212eee4131f158c463c4366
tanguy-ddv <tanguy.dugas01@gmail.com>
2025-03-23 19:24:11 +0100

    new: add the TextProgressBar to the widget list

--- pygaming/screen/widget/__init__.py
+++ pygaming/screen/widget/__init__.py
@@ -4,6 +4,6 @@ from .entry import Entry
 from .label import Label, Paragraph
 from .slider import Slider
 from .widget import Widget
-from .progress_bar import ProgressBar
+from .progress_bar import ProgressBar, TextProgressBar
 
-__all__ = ['Button', 'TextButton', 'Entry', 'Label', 'Slider', 'Widget', 'Paragraph', 'ProgressBar']
+__all__ = ['Button', 'TextButton', 'Entry', 'Label', 'Slider', 'Widget', 'Paragraph', 'ProgressBar', 'TextProgressBar']
